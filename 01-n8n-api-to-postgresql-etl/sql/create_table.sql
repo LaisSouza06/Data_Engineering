585c1875416c89b7185f0fb70b741e21ad05bf04
Laís Souza <laiscrsouza@gmail.com>
2026-08-01 18:43:37 -0300

    Create create_table.sql

---
+++ 01-n8n-api-to-postgresql-etl/sql/create_table.sql
@@ -0,0 +1,9 @@
+
+CREATE TABLE usuarios (
+    id INTEGER PRIMARY KEY,
+    nome VARCHAR(255),
+    username VARCHAR(255),
+    email VARCHAR(255),
+    cidade VARCHAR(255),
+    empresa VARCHAR(255)
+);
